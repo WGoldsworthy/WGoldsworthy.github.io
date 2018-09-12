e1f64fc80b7dde40358d23b95ca15575bb2c7eea
William Goldsworthy <wjgoldsworthy1@sheffield.ac.uk>
2018-09-12 16:28:48 +0100

    Electric Visualiser

---
+++ electric/js/audioSetup.js
@@ -0,0 +1,93 @@
+/*
+	Audio Setup
+	William Goldsworthy
+	09.03.2017
+*/
+
+var currentSong = 9;
+var playing = true;
+
+document.addEventListener("DOMContentLoaded", function(){
+
+	var audio = document.getElementById("myAudio");
+	var source = document.getElementById("mp3source");
+	source.src = "../audio/"+songNames[currentSong];
+	audio.load();
+	audio.play();
+
+	$('.songName').text(stripMp3(songNames[currentSong]));
+});
+
+var stripMp3 = function(songname) {
+	return songname.slice(0, songname.length - 4);
+}
+
+$('.selectLeft').click(function() {
+
+	currentSong -= 1;
+
+	if (currentSong < 0) {
+		currentSong = songNames.length - 1;
+	}
+
+	var audio = document.getElementById("myAudio");
+	var source = document.getElementById("mp3source");
+	source.src = "../audio/"+songNames[currentSong];
+	audio.load();
+	audio.play();
+
+	$('.songName').text(stripMp3(songNames[currentSong]));
+});
+
+$('.selectRight').click(function() {
+
+	currentSong += 1;
+
+	if (currentSong >= songNames.length) {
+		currentSong = 0;
+	}
+
+	var audio = document.getElementById("myAudio");
+	var source = document.getElementById("mp3source");
+	source.src = "../../Visualizer/audio/"+songNames[currentSong];
+	audio.load();
+	audio.play();
+
+	$('.songName').text(stripMp3(songNames[currentSong]));
+});
+
+var audio = document.getElementById("myAudio");
+var source = document.getElementById("mp3source");
+
+$('.jumpForward').click(function() {
+	audio.currentTime += 30;
+});
+
+$('.jumpBack').click(function() {
+	if (audio.currentTime < 30) {
+		audio.currentTime = 0;
+	} else {
+		audio.currentTime -= 30;
+	}
+});
+
+$('.songName').hover(function(){
+		if (playing) {
+			$('.songName').text('Pause');
+		} else {
+			$('.songName').text("Play");
+		}
+	}, function() {
+		$('.songName').text(stripMp3(songNames[currentSong]));
+	}
+);
+
+$('.songName').click(function(){
+	if (playing) {
+		playing = false;
+		audio.pause();
+	} else {
+		playing = true;
+		audio.play();
+	}
+});
---
+++ electric/js/index.js
@@ -0,0 +1,326 @@
+var opacityFill = 0.01;
+var samplingInterval = 12;
+var c;
+var canvas;
+var sample;
+var audiosource;
+var numParticles;
+var distanceRange;
+var noise;
+var velocity;
+var colors;
+var colorCycle = 0;
+
+var particle;
+
+window.onload = function()
+{
+    // Initital Setup
+
+    audiosource = new AudioSource();
+    
+    canvas = document.querySelector('canvas');
+    
+    c = canvas.getContext('2d');
+    
+    canvas.width = innerWidth;
+    
+    canvas.height = innerHeight;
+    
+    // Variables
+    
+    var mouse = 
+    {
+        x: innerWidth / 2,
+        
+        y: innerHeight / 2
+    };
+
+    
+    samplingInterval = 12;
+    var distanceOffset = 1.5;
+    velocity = 0.01;
+    var multiDirection = true;
+    distanceRange = [1, 120];
+    numParticles = 60;
+    noise = 1;
+    var backgroundColor = "white";
+
+    colors = [
+        [
+            '#ED3312',
+            '#0E1428',
+            '#7B9E89',
+        ],
+        [
+            '#2660A4',
+            '#F19953',
+            '#56351E',
+        ],
+        [
+            '#9C528B',
+            '#1D1E2C',
+            '#E534BC',
+        ],
+        [
+            '#323031',
+            '#FFC857',
+            '#DB3A34'
+        ],
+        [
+            '#4CA526',
+            '#3C787E',
+            '#FF7F11',
+        ]
+    ]
+    
+    
+    addEventListener('resize', function()
+    {
+        canvas.width = innerWidth;
+        
+        canvas.height = innerHeight;
+        
+        particle(numParticles);
+    });
+    
+    // Utility Functions
+    
+    function randomIntFromRange(min, max)
+    {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+    
+    function randomColor(colors)
+    {
+        return colors[Math.floor(Math.random() * colors.length)];
+    }
+    
+    // Objects
+    
+    function Particle(x, y, radius, color)
+    {
+        this.x = x;
+        
+        this.y = y;
+        
+        this.radius = radius;
+        
+        this.color = color;
+        
+        this.radians = Math.random() * Math.PI * 2;
+        
+        var plusOrMinus = Math.random() < 0.5 ? -1 : 1;
+        
+        // this.velocity = 0.05;
+        this.velocity = Math.random() * velocity * plusOrMinus;
+        
+        this.updateVelocity = function() {
+            this.velocity = Math.random() * velocity * plusOrMinus;
+        }
+
+        this.initialDistance = randomIntFromRange(distanceRange[0], distanceRange[1]);
+        
+        this.lastMouse = {x: x, y: y};
+        
+        this.update = function(num)
+        {
+            const lastPoint = {x: this.x, y: this.y}; // Taking the last point before we re-draw.
+            
+
+            var dist = Array.prototype.slice.call(audiosource.streamData)[num];
+
+            this.distanceFromCenter = this.initialDistance + (dist/noise);
+
+            //Move points over time
+            
+            this.radians += this.velocity;
+            
+            // Drag Effect
+            
+            this.lastMouse.x += (mouse.x - this.lastMouse.x) * 0.05;
+            
+            this.lastMouse.y += (mouse.y - this.lastMouse.y) * 0.05;
+            
+            // Circular Motion
+            
+            this.x = this.lastMouse.x + Math.cos(this.radians) * this.distanceFromCenter;
+            
+            this.y = this.lastMouse.y + Math.sin(this.radians) * this.distanceFromCenter;
+            
+            this.draw(lastPoint);
+        };
+        
+        this.draw = function(lastPoint)
+        {
+            c.beginPath();    
+            
+            c.strokeStyle = this.color;
+            
+            c.lineWidth = radius;
+            
+            c.moveTo(lastPoint.x, lastPoint.y);
+            
+            c.lineTo(this.x, this.y);
+            
+            c.stroke();
+            
+            c.closePath();
+        };
+    }
+    
+    // Implementation
+    
+    let particles;
+    
+    particle = function init(numParticles)
+    {
+        particles = [];
+        
+        for(let i = 0; i < numParticles; i++)
+        {
+            const radius = (Math.random() * 2) + 1;
+            
+            particles.push(new Particle(canvas.width / 2, canvas.height / 2, 1, randomColor(colors[colorCycle])));
+        }
+    }
+    
+    // Animation Loop
+    
+    function animate()
+    {
+        requestAnimationFrame(animate);
+        
+        // c.clearRect(0, 0, canvas.width, canvas.height);
+        
+        // c.fillStyle = 'rgba(255, 255, 255, 0.01)';
+
+        var fillstyle = 'rgba(255, 255, 255, ' + opacityFill + ')';
+
+        // var fillstyle = 'rgba(0, 0, 0, ' + opacityFill + ')';
+
+
+        c.fillStyle = fillstyle;
+        
+        c.fillRect(0, 0, canvas.width, canvas.height);
+        var i = 0;
+        particles.forEach(particle => 
+        {
+            particle.update(i);
+            i++;
+        });
+    }
+    
+    // Object calls
+    
+    // Function calls
+    
+    particle(numParticles);
+    
+    animate();
+};
+
+var AudioSource = function() {
+    var self = this;
+    var ctx = new AudioContext();
+    var audio = document.getElementById('myAudio');
+
+    audio.crossOrigin = "anonymous";
+
+      var audioSrc = ctx.createMediaElementSource(audio);
+      var analyser = ctx.createAnalyser();
+      // we have to connect the MediaElementSource with the analyser 
+      audioSrc.connect(analyser);
+      audioSrc.connect(ctx.destination);
+      // we could configure the analyser: e.g. analyser.fftSize (for further infos read the spec)
+     
+      // frequencyBinCount tells you how many values you'll receive from the analyser
+      self.frequencyData = new Uint8Array(analyser.frequencyBinCount);
+
+      this.volume = 0;
+
+      this.streamData = new Uint8Array(100);
+
+      this.sampleAudioStream = function() {
+            analyser.getByteFrequencyData(self.streamData);
+            // calculate an overall volume value
+            var total = 0;
+            for (var i = 0; i < 80; i++) { // get the volume from the first 80 bins, else it gets too loud with treble
+                total += self.streamData[i];
+            }
+            self.volume = total;
+      };
+      sample = setInterval(this.sampleAudioStream, samplingInterval);
+      audio.play();
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    
+    document.getElementById('opacity_slider').oninput = function(){
+        // Change value 1-100 into range 0.001-0.2
+
+        c.clearRect(0, 0, canvas.width, canvas.height);
+        opacityFill = this.value / 1000
+        if (this.value <= 1) {
+            $('body').css('color', 'white');
+        } else {
+            $('body').css('color', 'black');
+        }
+    }
+
+     document.getElementById('sampling_slider').oninput = function(){
+
+        samplingInterval = this.value
+        clearInterval(sample);
+        sample = setInterval(audiosource.sampleAudioStream, samplingInterval)
+      }
+
+      document.getElementById('particles_slider').oninput = function(){
+
+        c.clearRect(0, 0, canvas.width, canvas.height);
+        numParticles = this.value
+        particle(numParticles);
+      }
+
+      document.getElementById('lowerDistance_slider').oninput = function(){
+
+        c.clearRect(0, 0, canvas.width, canvas.height);
+        distanceRange[0] = this.value
+        particle(numParticles);
+      }
+
+      document.getElementById('higherDistance_slider').oninput = function(){
+
+        c.clearRect(0, 0, canvas.width, canvas.height);
+        distanceRange[1] = this.value
+        particle(numParticles);
+      }
+
+      document.getElementById('noise_slider').oninput = function(){
+
+        noise = this.value
+      }
+
+      document.getElementById('velocity_slider').oninput = function(){
+
+        // c.clearRect(0, 0, canvas.width, canvas.height);
+        velocityNorm = this.value/400
+        velocity = velocityNorm
+        particle(numParticles);
+      }
+
+      document.getElementById('colors').addEventListener("click", function() {
+
+        colorCycle += 1;
+
+        // colorCycle = Math.floor(Math.random() * colors.length)
+
+        if (colorCycle >= colors.length) {
+            colorCycle = 0
+        }
+
+        particle(numParticles);
+      });
+
+  });
+
